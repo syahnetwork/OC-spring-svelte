cc1793b35d6114d5897da4b7d211c11a9feb5817
Adam <syahnetwork@gmail.com>
2020-07-24 08:45:18 +0700

    #6 solve error deploying .war

--- src/main/java/com/ocsvelte/clinica/ClinicaApplication.java
+++ src/main/java/com/ocsvelte/clinica/ClinicaApplication.java
@@ -2,12 +2,18 @@ package com.ocsvelte.clinica;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ClinicaApplication {
+public class ClinicaApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ClinicaApplication.class, args);
-	}
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(ClinicaApplication.class);
+    }
 
+    public static void main(String[] args) {
+        SpringApplication.run(ClinicaApplication.class, args);
+    }
 }
